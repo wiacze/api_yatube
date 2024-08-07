b1b7020d3fbbed1b0d19f6cfdb7bf9222834345e
wiacze <151467867+wiacze@users.noreply.github.com>
2024-08-07 05:43:57 +0300

    Author Field class changed

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -3,22 +3,24 @@ from rest_framework import serializers
 from posts.models import Group, Post, Comment
 
 
+class AuthorFieldMixin(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username')
+
+
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = ('id', 'title', 'slug', 'description')
 
 
-class PostSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField(read_only=True)
-
+class PostSerializer(AuthorFieldMixin):
     class Meta:
         model = Post
         fields = ('id', 'text', 'author', 'image', 'group', 'pub_date')
 
 
-class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField(read_only=True)
+class CommentSerializer(AuthorFieldMixin):
     post = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
